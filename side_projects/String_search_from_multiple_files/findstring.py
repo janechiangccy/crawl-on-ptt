fbfacb4eea5cec85e71b3a19a3655d7119a17759
timtsai <theblazertim1020@gmail.com>
2023-03-02 22:20:07 +0800

    first upload

---
+++ side_projects/String_search_from_multiple_files/findstring.py
@@ -0,0 +1,40 @@
+import os
+
+text = input('input text: ')
+path = input('path: ')
+
+def getfiles(path):
+    f = 0
+    os.chdir(path) #os.chdir 用於改變當前工作目錄到指定的路徑, path 為新的路徑  //os.getcwd 是獲得當下在運行 python 的路徑
+    files = os.listdir() #os.listdir 用於回傳指定的文件夾的所有文件和文件夾之列表
+    #print(filses)
+
+    for file_name in files:
+        abs_path = os.path.abspath(file_name) #abs_path 絕對路徑
+        if os.path.isdir(abs_path): #判斷 abs_path 是否為目錄
+            getfiles(abs_path) #若 True, 則執行getfiles
+        if os.path.isfile(abs_path): #判斷 abs_path 是否為文件
+            f = open(file_name, 'rt', encoding='utf-8',errors='ignore') #若 True, 則打開此文件, 並查找文件內的text
+            if text in f.read(): #若 text 有在文件內
+                f = 1 
+                print(text + ' found in ')
+                final_path = os.path.abspath(file_name)
+                print(final_path)
+                return True
+            else: 
+                f = 0
+
+    if f == 0: 
+        print(text +' not found! ')
+        return False
+
+getfiles(path)
+
+
+#error: UnicodeDecodeError >> f = open(file_name, 'rt', encoding='utf-8',errors='ignore') //https://ithelp.ithome.com.tw/articles/10194277 , https://stackoverflow.com/questions/42339876/error-unicodedecodeerror-utf-8-codec-cant-decode-byte-0xff-in-position-0-in
+#FileNotFoundError >> 貼上完整的路徑
+#os.相關模組之用法 //https://www.runoob.com/python/python-os-path.html , https://blog.gtwang.org/programming/python-list-all-files-in-directory/
+# not found 之情境? OK
+#Q: 找出所有有出現 text 之文件
+
+
